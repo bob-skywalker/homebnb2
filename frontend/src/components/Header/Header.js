3a8713d18cc8716ea39d7d85675124d001eed60e
bob-skywalker <q463250938@gmail.com>
2022-09-05 16:27:00 -0700

    header added

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,21 +1,37 @@
 import React from 'react'
 import Navigation from '../Navigation'
 import './Header.css'
-
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ViewHeadlineIcon from '@mui/icons-material/ViewHeadline';
+import { Avatar } from '@mui/material';
 
 
 const Header = () => {
   return (
     <>
         <div className='header'>
-            <img className='header-icon'
-            src='https://i.postimg.cc/nVXJ207G/kisspng-airbnb-logo-coupon-privately-held-company-airbnb-logo-5b167f0c91db57-0436925715282009725974.png' alt='header-img'/>
-            <div className='header-center'>
-            <input type='text'/>
-            <img className='magnifying-glass'
-            src='https://cdn-icons-png.flaticon.com/512/3466/3466268.png' alt='header-img'/>
+            <div className='header-left'>
+              <img className='header-icon'
+              src='https://i.postimg.cc/nVXJ207G/kisspng-airbnb-logo-coupon-privately-held-company-airbnb-logo-5b167f0c91db57-0436925715282009725974.png' alt='header-img'/>
             </div>
-            <Navigation />
+
+            <div className='header-center'>
+              <input type='text'/>
+              <SearchIcon /> 
+            </div>
+
+            <div className='header-right'>
+              <p className='header-text'>Become a Host</p>
+              <LanguageIcon className='header-globe'/>
+              <div className='header-profile'>
+                <ViewHeadlineIcon /> 
+                <Avatar className='header-avatar'/>
+              </div>
+              {/* <Navigation /> */}
+            </div>
         </div>
     </>
   )
