7498db024e1e3d9962fddd22acc909032647a34b
bob-skywalker <q463250938@gmail.com>
2022-09-06 22:10:56 -0700

    added search date button

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -1,12 +1,20 @@
 import { Button } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 
 const Banner = () => {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className='banner'>
+      <div className='banner-search'>
+        {showSearch && <h1>SHOW DATE PICKER</h1>}
+        <Button className='banner-searchButton'
+          variant='outlined'>Search Dates
+        </Button>
+      </div>
       <div className='banner-info'>
-        <h1>Go explore the unthinkable!</h1>
+        <h1>Explore the Unthinkable!</h1>
           <h3>
             Plan your next travel and find out your favourite hideout places near you.
           </h3>
