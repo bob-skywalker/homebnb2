fd279bcf64c791f02b8e427625d066ddbd601f1d
bob-skywalker <q463250938@gmail.com>
2022-09-07 17:33:21 -0700

    added listing

---
+++ db/migrate/20220907231158_create_listings.rb
@@ -0,0 +1,26 @@
+class CreateListings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :listings do |t|
+      t.string :title, null:false , index:{unique: true}
+      t.string :description, null: false
+      t.string :street_address, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.integer :zip_code, null:false
+      t.string :country, null: false
+      t.string :region, null: false
+      t.float :lat, null:false
+      t.float :lng, null:false
+      t.string :currency, null: false, default: 'USD'
+      t.float :price, null: false
+      t.float :other_fees
+      t.float :other_fees_type
+      t.integer :num_bedrooms, null:false
+      t.integer :num_beds, null:false
+      t.integer :num_baths, null: false
+      t.references :host, foreign_key:{to_table: :users}
+      t.boolean :is_posted, null:false, default: false 
+      t.timestamps
+    end
+  end
+end
