72f764cb6f1bd81a6561bacb4f94127c410c2f16
bob-skywalker <q463250938@gmail.com>
2022-09-15 09:47:34 -0700

    price calculator styled

--- frontend/src/components/RangePicker/RangePicker.js
+++ frontend/src/components/RangePicker/RangePicker.js
@@ -3,12 +3,20 @@ import './RangePicker.css'
 import { DateRangePicker } from 'react-date-range'
 import 'react-date-range/dist/styles.css'
 import 'react-date-range/dist/theme/default.css'
-import { Box, TextField } from '@mui/material'
+import { Box, Button, TextField } from '@mui/material'
+import { PeopleOutline } from '@material-ui/icons'
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import { useHistory, useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { getListing } from '../../store/listings'
 
 
 const RangePicker = () => {
+  const {listingId} = useParams();
   const[startDate, setStartDate] = useState(new Date())
   const[endDate, setEndDate] = useState(new Date())
+  const history = useHistory()
+  const listing = useSelector(getListing(listingId))
 
   const selectionRange = {
     startDate: startDate,
@@ -24,14 +32,33 @@ const RangePicker = () => {
   console.log(selectionRange)
 
   return (
-    <div className='range-picker'>
-        <DateRangePicker
-        className='rp'
-        ranges={[selectionRange]}
-        onChange={handleSelect}
-        minDate = {new Date()}
-        />
-    </div>
+    <>
+      <div className='range-picker'>
+            <DateRangePicker
+            className='rp'
+            ranges={[selectionRange]}
+            onChange={handleSelect}
+            minDate = {new Date()}
+            />
+            <h2>
+              Number of guests
+              <PeopleOutlineIcon/>
+            </h2>
+            <input
+            defaultValue={2}
+            type='number'
+            />
+            <Button variant='outlined' onClick={()=> history.pushState('/search')}>Reserve</Button>
+            <div className='price-calculator'>
+              <p className='warning'>You won't be charged yet</p>
+              <div className='price-sums'>
+                <h3>{listing.price}</h3>
+                <h3>${listing.price}</h3>
+              </div>
+              <div></div>
+            </div>
+      </div>
+    </>
   )
 }
 
